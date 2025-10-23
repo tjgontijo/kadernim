10cbd3d7df9face865a71270d55f588b623e0e0f
Thiago Gontijo <tjgontijo@gmail.com>
2025-10-23 20:58:36 -0300

    Update auth.ts

--- src/lib/auth/auth.ts
+++ src/lib/auth/auth.ts
@@ -1,4 +1,4 @@
-import { betterAuth, email } from 'better-auth'
+import { betterAuth } from 'better-auth'
 import { prismaAdapter } from 'better-auth/adapters/prisma'
 import { prisma } from '../prisma'
 import { magicLink } from 'better-auth/plugins'
