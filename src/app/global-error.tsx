e80c42c87f15c23b0716d051e152d20ba4be3371
Thiago Gontijo <tjgontijo@gmail.com>
2026-01-03 19:02:03 -0300

    Update global-error.tsx

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -10,8 +10,8 @@ export default function GlobalError({
   return (
     <html>
       <body>
-        <h2>Something went wrong!</h2>
-        <button onClick={() => reset()}>Try again</button>
+        <h2>Algo de errado aconteceu!</h2>
+        <button onClick={() => reset()}>Tente novamente</button>
       </body>
     </html>
   )
